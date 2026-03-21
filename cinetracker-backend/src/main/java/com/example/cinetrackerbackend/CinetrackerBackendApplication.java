00c556af4a03b5c1199836ea0dd0b810a517fb94
mayank0201 <mayankmadhavi14@gmail.com>
2026-03-21 21:44:59 +0530

    feat: setup JWT config and initial security structure

---
+++ cinetracker-backend/src/main/java/com/example/cinetrackerbackend/CinetrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinetrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinetrackerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinetrackerBackendApplication.class, args);
+    }
+
+}
---
+++ cinetracker-backend/src/main/java/com/example/cinetrackerbackend/config/JwtConfig.java
@@ -0,0 +1,15 @@
+package com.example.cinetrackerbackend.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "jwt")
+@Data
+public class JwtConfig {
+
+    private String secret; //since prefix jwt used , it will map to jwt.secret from app.prop
+    private long expiration;
+
+}
